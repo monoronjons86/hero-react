3c625763fb890f0f697e1f844905b955458d0dcf
monoronjon sarker <monoronjons86@gmail.com>
2023-10-20 11:27:28 +0600

    component

--- ema-john-simple/src/App.jsx
+++ ema-john-simple/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Component/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
+    <div>
+      <Header></Header>
+    </div>
   )
 }
 
