7d3406c948e3bdb9a7e8173c995899bb14c057b2
monoronjon sarker <monoronjons86@gmail.com>
2023-10-20 17:14:52 +0600

    dd

--- ema-john-simple/src/Component/Header/Header.jsx
+++ ema-john-simple/src/Component/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
+  return (
+    <div className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/Shop">Shop</a>
+        <a href="/Order">Order</a>
+        <a href="/Inventory">Inventory</a>
+        <a href="/Login">Login</a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
