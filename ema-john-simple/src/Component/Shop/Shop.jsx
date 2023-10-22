c1a78e9f9c25f68c260a1d2900ec6b8842664d2a
monoronjon sarker <monoronjons86@gmail.com>
2023-10-22 23:04:32 +0600

    data load-added

--- ema-john-simple/src/Component/Shop/Shop.jsx
+++ ema-john-simple/src/Component/Shop/Shop.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const[products,setProduct]=useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProduct(data))
+    },[])
     return (
         <div className='shop-container'>
           <div className='products-container'>
-            <h2>Products coming here</h2>
+            <h2>Products coming here:{products.length}</h2>
             </div> 
             <div className='cart-container'>
                 <h4>Order summary</h4>
