2a389eb9089a12da988257b6045a900790c088f4
monoronjon sarker <monoronjons86@gmail.com>
2023-10-22 20:33:54 +0600

    try to shop add

--- ema-john-simple/src/App.jsx
+++ ema-john-simple/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
---
+++ ema-john-simple/src/Component/Shop/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h2>Products coming here</h2>
+            </div>
+            <div className='cart-container'>
+                <H4>Order summary</H4>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Shop;
